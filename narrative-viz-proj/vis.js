40dae27cced5b14ec7490a056ab72b9859d84b4f
naveenkcb <naveenkcb@gmail.com>
2024-07-24 15:37:58 -0500

    data

--- narrative-viz-proj/vis.js
+++ narrative-viz-proj/vis.js
@@ -110,7 +110,8 @@ var bar_tooltip = d3.select("body")
     .style("color", "white")
 
 async function load1() {
-    d3.csv("https://flunky.github.io/cars2017.csv").then(function (data_given) {
+    //d3.csv("https://flunky.github.io/cars2017.csv").then(function (data_given) {
+    d3.csv("https://naveenkcb.github.io/narrative-viz-proj/world-data-2023.csv").then(function (data_given) {
         // var makes = [];
         // var highway_mpgs = [];
         // var city_mpgs = []
